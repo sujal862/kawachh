7ff87992b7c00b65cd4976465fa6aa2651ff3d6d
Sujal Raj <rajsujal2004@gmail.com>
2025-04-17 12:09:58 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -19,8 +19,8 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true
+    origin: true,  // Allow all origins
+    credentials: false  
 }));
 
 app.use(express.json());
